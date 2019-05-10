28b481851ef03d59627216f43805eb1d6aeb6f4e
Neger008 <alfredo.full.cf@gmail.com>
2019-05-10 09:58:01 -0500

    se agrego el metodo modificar Persona

--- src/main/java/com/parcial/examen/controller/PersonController.java
+++ src/main/java/com/parcial/examen/controller/PersonController.java
@@ -1,12 +1,17 @@
 package com.parcial.examen.controller;
 
 import com.parcial.examen.model.Person;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
+@RequiredArgsConstructor
 public class PersonController {
-    @PostMapping("/agregar")
+    private final List<Person> listPerson;
+    @PostMapping("/save")
     public String postSavePerson(@RequestBody Integer id, @RequestBody String name, @RequestBody String last_name,
                                  @RequestBody String dni, @RequestBody String mobile){
         Person person = new Person();
@@ -15,6 +20,23 @@ public class PersonController {
         person.setLast_name(last_name);
         person.setDni(dni);
         person.setMobile(mobile);
+
+        listPerson.add(person);
+        return "saved successfully";
+    }
+    public String postUpdatePerson(@RequestBody Integer id, @RequestBody String name, @RequestBody String last_name,
+                                   @RequestBody String dni, @RequestBody String mobile) {
+
+        for (Person person1 :
+                listPerson) {
+            if (person1.getDni().equals(dni)) {
+                person1.setId(id);
+                person1.setName(name);
+                person1.setLast_name(last_name);
+                person1.setMobile(mobile);
+            }
+
+        }
         return "saved successfully";
     }
 }
