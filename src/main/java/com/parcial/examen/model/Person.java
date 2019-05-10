e5a08d16f0e0d136364d8470cb98a899706d8714
Neger008 <alfredo.full.cf@gmail.com>
2019-05-10 09:14:26 -0500

    se creo la clase persona

---
+++ src/main/java/com/parcial/examen/model/Person.java
@@ -0,0 +1,12 @@
+package com.parcial.examen.model;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private Integer id;
+    private String name;
+    private String last_name;
+    private String dni;
+    private String mobile;
+}
